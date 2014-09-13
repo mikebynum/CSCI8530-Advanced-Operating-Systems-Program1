a448112a120304e7d6daa7c2aafbd1e5b40a5dbb
Mike Bynum <mikebynum@Mikes-MacBook-Air.local>
2014-09-12 19:27:06 -0500

    Initial Commit

---
+++ CSCI8530-Program1/main.c
@@ -0,0 +1,18 @@
+//
+//  main.c
+//  CSCI8530-Program1
+//
+//  Created by Mike Bynum on 9/12/14.
+//  Copyright (c) 2014 Mike Bynum. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
+
